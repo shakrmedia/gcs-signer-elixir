ac6941ff18710bda3c8afe615b77ecd53a73eae3
Minku Lee <minku@sha.kr>
2018-07-06 16:06:52 +0900

    Version bump - release 0.2.0

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule GcsSigner.Mixfile do
       app: :gcs_signer,
       description: "Simple signed URL generator for Google Cloud Storage",
       package: package(),
-      version: "0.1.1",
+      version: "0.2.0",
       elixir: "~> 1.5",
       start_permanent: Mix.env == :prod,
       deps: deps(),
